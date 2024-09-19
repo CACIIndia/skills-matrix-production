5d865cc1ea4226c3fbf2b79b374c55706882908a
ahersi <ahersi@caci.co.uk>
2024-09-19 11:23:11 +0100

    Reorganise folders and basic cleanup

---
+++ src/constants/header.tsx
@@ -0,0 +1,21 @@
+export const MENU_ITEMS = [
+	{
+		name: "Dashboard",
+		path: "/dashboard",
+		icon: "ki-element-11",
+		tab: "/dashboard",
+	},
+	{
+		name: "Public Profile",
+		path: "/public-profile",
+		icon: "ki-profile-circle ",
+		tab: "/public-profile",
+	},
+];
+
+export const PROFILE_HEADER_ITEMS = [
+	{name: "Overview", path: "/my-accounts/overview"},
+	{name: "Skills", path: "/my-accounts/skills"},
+	// {name:"Project",path:"/my-accounts/project"},
+	{name: "Documents", path: "/my-accounts/documents"},
+];
