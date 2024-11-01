a31ad5de8e9d03c2f9520ff13e5ca0bdb663cc50
ahersi <ahersi@caci.co.uk>
2024-11-01 11:25:45 +0000

    Fix profile image not loading and toast messages on CRUD operations

--- src/lib/hooks/profile/useCertificateHandlers.ts
+++ src/lib/hooks/profile/useCertificateHandlers.ts
@@ -1,8 +1,10 @@
+import toast from "react-hot-toast";
 import { useQueryClient } from "@tanstack/react-query";
-import { fileToBase64 } from "@/lib/utils/fileToBase64";
+
 import { addCertificate } from "@/app/actions/certificate/addCertificate";
-import { updateCertificate } from "@/app/actions/certificate/updateCertificate";
 import { deleteCertificate } from "@/app/actions/certificate/deleteCertificate";
+import { updateCertificate } from "@/app/actions/certificate/updateCertificate";
+import { fileToBase64 } from "@/lib/utils/fileToBase64";
 
 export const useCertificateHandlers = (userId: string) => {
   const queryClient = useQueryClient();
@@ -14,82 +16,102 @@ export const useCertificateHandlers = (userId: string) => {
   const handleUpload = async (certificate: any) => {
     const { certificateFile, ...rest } = certificate;
 
+    const toastId = toast.loading("Uploading certificate...");
+
     try {
       const base64Certificate = await fileToBase64(certificate.certificateFile);
-      await addCertificate({
+
+      const result = await addCertificate({
         ...rest,
         base64Certificate,
         createdBy: userId,
       });
+
       invalidate();
+      toast.success(result.message, { id: toastId });
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred.";
+      const errorMessage =
+        error instanceof Error ? error.message : "An unknown error occurred.";
       console.error("Error uploading certificate:", errorMessage);
-      alert(errorMessage);
+      toast.error(errorMessage, { id: toastId });
     }
-    
   };
 
   const handleEdit = async (id: number, updatedData: any) => {
+    const toastId = toast.loading("Updating certificate...");
+
     try {
-      await updateCertificate(String(id), {
+      const result = await updateCertificate(String(id), {
         ...updatedData,
         createdBy: userId,
       });
-      invalidate();
-    } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred.";
-      console.error("Error updating certificate:", errorMessage);
-      alert(errorMessage);
-    }    
-  };
 
-  const handleDelete = async (id: number) => {
-    try {
-      await deleteCertificate(String(id));
       invalidate();
-    }catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred.";
-      console.error("Error deleting certificate:", errorMessage);
-      alert(errorMessage);
+      toast.success(result.message, { id: toastId });
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "An unknown error occurred.";
+      console.error("Error updating certificate:", errorMessage);
+      toast.error(errorMessage, { id: toastId });
     }
   };
 
-  const handleDownload = async (fileUrl: string, createdById: string, fileName: string) => {
+  const handleDelete = async (id: number) => {
+    const toastId = toast.loading("Deleting certificate...");
+
     try {
-      const response = await fetch(`https://smempprofile.blob.core.windows.net/certificatestorage/${fileUrl}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/pdf" },
-      });
-     
-  
+      const result = await deleteCertificate(String(id));
+
+      invalidate();
+      toast.success(result.message, { id: toastId });
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "An unknown error occurred.";
+      console.error("Error deleting certificate:", errorMessage);
+      toast.error(errorMessage, { id: toastId });
+    }
+  };
+
+  const handleDownload = async (
+    fileUrl: string,
+    createdById: string,
+    fileName: string,
+  ) => {
+    try {
+      const response = await fetch(
+        `https://smempprofile.blob.core.windows.net/certificatestorage/${fileUrl}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/pdf" },
+        },
+      );
+
       if (!response.ok) throw new Error("Failed to download the certificate.");
-  
+
       const fileBlob = await response.blob();
       const downloadLink = window.URL.createObjectURL(fileBlob);
       const formattedFileName = `${fileName.toLowerCase().replace(/\s+/g, "-")}-${createdById}-certificate.pdf`;
-  
+
       const link = document.createElement("a");
       link.href = downloadLink;
       link.download = formattedFileName;
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-  
+
       window.URL.revokeObjectURL(downloadLink);
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : "An unknown error occurred.";
+      const errorMessage =
+        error instanceof Error ? error.message : "An unknown error occurred.";
       console.error("Error downloading certificate:", errorMessage);
       alert(errorMessage);
     }
   };
-  
-  
 
   return {
     handleUpload,
     handleEdit,
     handleDelete,
-    handleDownload, 
+    handleDownload,
   };
 };
