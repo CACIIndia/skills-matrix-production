1a958ebd4802e43991c16f36e47d7d439e1fec6e
V A Magendran <95860580+Magendran001@users.noreply.github.com>
2024-12-02 14:31:02 +0530

    remove constant

--- src/app/profile/training/page.tsx
+++ src/app/profile/training/page.tsx
@@ -1,54 +1,10 @@
 "use client";
 
 import { useAppContext } from "@/app/context/AppContext";
-
-import ProfileAdditionalInfo from "@/components/views/profile/AdditionalInfo";
 import TrainingInfoCard from "@/components/views/profile/TrainingInfo";
-import ProfileProjectHistory from "@/components/views/profile/ProjectHistory";
-import ProfileSkills from "@/components/views/profile/Skills";
-
-import { DEFAULT_USER_DETAILS } from "@/lib/constants/profile";
-import { newDate } from "react-datepicker/dist/date_utils";
 import TrainingList from "@/components/views/profile/TrainingList";
 import { filterTrainings } from "@/lib/utils/trainingFilter";
-const trainings = [
-  {
-    id: "1",
-    categoryName: "Software Development",
-    skillName: "React.js",
-    fromDate: "2024-01-10",
-    tentativeEndDate: "2024-01-20",
-    description: "Advanced React.js training for senior developers.",
-    status: "Completed",
-  },
-  {
-    id: "2",
-    categoryName: "Project Management",
-    skillName: "Agile Methodologies",
-    fromDate: "2024-02-01",
-    tentativeEndDate: "2024-02-15",
-    description: "Agile principles and Scrum practices.",
-    status: "In Progress",
-  },
-  {
-    id: "3",
-    categoryName: "Software Development",
-    skillName: "React.js",
-    fromDate: "2024-01-10",
-    tentativeEndDate: "2024-01-20",
-    description: "Advanced React.js training for senior developers.",
-    status: "Completed",
-  },
-  {
-    id: "4",
-    categoryName: "Project Management",
-    skillName: "Agile Methodologies",
-    fromDate: "2024-02-01",
-    tentativeEndDate: "2024-02-15",
-    description: "Agile principles and Scrum practices.",
-    status: "In Progress",
-  },
-];
+
 
 
 
@@ -56,14 +12,10 @@ const trainings = [
 const Training = () => {
   const { profile } = useAppContext();
 
-  const projects = profile?.projects || [];
-
   const { currentInProgress, otherTrainings } = filterTrainings(
     profile?.trainingEmployees || []
   );
     
-  console.log(otherTrainings,"currentInProgresscurrentInProgress");
- 
 
   return (
     <div className=" ">
