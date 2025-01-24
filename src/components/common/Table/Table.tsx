eb5d6249af79bfd63955261773291701f22401b8
Kalyani Sahu <ksahu@caci.co.uk>
2025-01-24 12:55:17 +0530

    Added another color to the more button

--- src/components/common/Table/Table.tsx
+++ src/components/common/Table/Table.tsx
@@ -267,7 +267,7 @@ const Table = <T,>({
                                       "_blank",
                                     )
                                   }
-                                  className='text-nowrap bg-blue-600 h-[32px] w-[32px] rounded-[50%] text-white transition duration-300 hover:bg-blue-500'
+                                  className='text-nowrap bg-purple-800 h-[32px] w-[32px] rounded-[50%] text-white transition duration-300 hover:bg-purple-700'
                                 >
                                   +{row.userSkills.length - 2} 
                                 </button>
