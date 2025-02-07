8fc3f3ac58817d884dbb92a258434934b9b0175c
CACI India <support_india@caci.co.uk>
2025-02-07 12:31:31 +0530

    added project migration file

---
+++ prisma/migrations/20250207065901_add_project_role_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "ProjectRole" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProjectRole_pkey" PRIMARY KEY ("id")
+);
