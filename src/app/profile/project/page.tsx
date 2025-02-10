99e74740a59f3ec1c84786d4ed6575bef93fa9e0
CACI India <support_india@caci.co.uk>
2025-02-10 14:31:04 +0530

    modify profile history card appcontext

--- src/app/profile/project/page.tsx
+++ src/app/profile/project/page.tsx
@@ -2,9 +2,11 @@
 
 import Modal from "@/components/common/Modal";
 import Image from "next/image";
-import { useState } from "react";
+import {  useState } from "react";
 import AddProject from "./AddProject";
 import useGetProjects from "@/lib/hooks/profile/projects/useGetProjects";
+import { useAppContext } from "@/app/context/AppContext";
+import ProjectHistoryCard from "@/components/views/profile/ProjectHistory";
 interface ActivityItemProps {
   icon: string;
   title: string;
@@ -38,29 +40,46 @@ interface ActivityItemWithContentProps {
 }
 export default function ProjectPage() {
   const { data: projects } = useGetProjects();
+  const {profile} = useAppContext();
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className='flex h-[40vh] items-center justify-center'>
-      <button
+    <>
+      {
+        profile?.projects?.length === 0 && (
+          <div className='flex h-[40vh] items-center justify-center'>
+          <button
         onClick={() => setIsOpen(true)}
         className='relative mb-[4px] mr-[10px] rounded-[4px] bg-[#0d6efd] p-[6px] text-white transition duration-300 hover:bg-blue-700'
       >
         <div className='flex items-center justify-center space-x-1'>
           <div>Add Project</div>
         </div>
-      </button>
+      </button></div>
+        )
+      }
+
+
+      {
+        profile?.projects?.length!==0&&<ProjectHistoryCard projects={profile?.projects || []}/>
+      }
+
+    
+      
       <Modal
         isOpen={isOpen}
         setIsOpen={setIsOpen}
         title='Add New Project'
         buttonText='Update'
         // handler={handleEdit}
-        customWidth='w-[100%] lg:w-[40%] h-[100%] lg:h-[50%]'
+        customWidth='w-[100%] lg:w-[40%] h-[100%] lg:h-auto'
         isFromAddProject={true}
       >
         <AddProject handleClose={() => setIsOpen(false)} projects={projects} />
       </Modal>
-    </div>
+    
+
+    
+    </>
   );
 }
 {
