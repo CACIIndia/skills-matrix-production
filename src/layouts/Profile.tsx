9be1644bc5fb702a3fc351193a23be763c394ee6
CACI India <support_india@caci.co.uk>
2025-02-25 12:41:53 +0530

    line manager redirection done

--- src/layouts/Profile.tsx
+++ src/layouts/Profile.tsx
@@ -36,7 +36,7 @@ const ProfileLayout = ({ children }: ProfileLayoutProps) => {
   return (
     <div className='relative w-full' style={{backgroundColor:"white"}}>
  
-      <div className="fixed top-0 " style={{backgroundColor:"white",width:"100%",height:"400px",zIndex:100,border:"1px solid red"}}>
+      <div className="fixed top-0 " style={{backgroundColor:"white",width:"100%",height:"400px",zIndex:100}}>
       {/* Banner */}
       <div className='banner  relative'  >
        
