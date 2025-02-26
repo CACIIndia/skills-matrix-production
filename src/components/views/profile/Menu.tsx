f9b5d8e11763299dc1c53829cc02b124c6933c1a
CACI India <support_india@caci.co.uk>
2025-02-26 15:20:33 +0530

    remove direct to skill tab

--- src/components/views/profile/Menu.tsx
+++ src/components/views/profile/Menu.tsx
@@ -6,25 +6,25 @@ import {
   PROFILE_MENU_ITEMS,
   VIEW_PROFILE_MENU_ITEMS,
 } from "@/lib/constants/header";
-import { useParams, useSearchParams } from "next/navigation";
+import { useParams } from "next/navigation";
 
 const ProfileMenu = () => {
   const { id } = useParams();
-  const searchParams = useSearchParams();
-  const tab = searchParams.get("tab");
+  // const searchParams = useSearchParams();
+  // const tab = searchParams.get("tab");
   const items = PROFILE_MENU_ITEMS;
   const [activePath, setActivePath] = useState(items[0]?.path || "");
   const [isScrolling, setIsScrolling] = useState(false);
   const scrollOffset = 400;
 
-  useEffect(()=>{
-    if(tab){
-      const activeItem = items.find((item) => item.name.includes(tab));
-      if(activeItem){
-        setActivePath(activeItem.path);
-      }
-    }
-  },[])
+  // useEffect(()=>{
+  //   if(tab){
+  //     const activeItem = items.find((item) => item.name.includes(tab));
+  //     if(activeItem){
+  //       setActivePath(activeItem.path);
+  //     }
+  //   }
+  // },[])
 
   const handleMenuClick = (path: string) => {
     setIsScrolling(true);
